c757b14ea82a960a88548f977dcc43d96e25e91d
dEVgenFis <Frozen_Vi@mail.ru>
2022-05-19 10:26:44 +0400

    Создана модель "PageInfo" - класс, содержащий в себе следующую информацию для работы с пагинацией: - номер текущей страницы; - количество объектов на странице; - общее количество объектов какой-либо коллекции; - общее количество страниц.

---
+++ OnlineShop/OnlineShopWebApp/Models/PageInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OnlineShopWebApp.Models
+{
+    public class PageInfo
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages
+        {
+            get
+            {
+                return (int)Math.Ceiling((double)TotalItems / PageSize);
+            }
+        }
+    }
+}
